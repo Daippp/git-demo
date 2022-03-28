126f0c2c82b251a6612a908e0eeb0affddf019ce
1423108003@qq.com <1423108003@qq.com>
2022-03-28 10:59:50 +0800

    fourth commit.3

--- loan.h
+++ loan.h
@@ -1,7 +1,14 @@
 #include<stdio.h>
 #include<string.h>
 
+//load loan form file
 void loadloan();
+
+//save loan to file
 void save_loan();
+
+//borrowbook
 void borrowbook();
+
+//return book
 void returnbook();
